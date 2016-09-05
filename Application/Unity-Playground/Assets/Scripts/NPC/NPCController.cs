15f9a454ab17ec6684ec482094c322ed0b673b31
= <=>
2016-09-04 22:47:31 -0400

    Unity - Added GenCameraController and UIController for HUD display

---
+++ Application/Unity-Playground/Assets/Scripts/NPC/NPCController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCController : MonoBehaviour, IPerceivable {
+    
+    #region AI
+    private NPCAI gAI;
+    #endregion
+
+    #region BODY
+    private NPCBody gBody;
+    #endregion
+
+    #region PERCEPTION
+    private NPCPerception gPerception;
+    #endregion
+
+    #region Properties
+    public NPCPerception Perception {
+        get { return gPerception; }
+    }
+
+    public NPCBody Body {
+        get { return gBody; }
+    }
+    #endregion
+
+    #region Unity_Runtime
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+    void FixedUpdate() {
+        gBody.UpdateBody();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // When script is added to GameObject or Reset
+    void Reset() {
+        Debug.Log("Resetting NPCController");
+        if (GetComponent<NPCBody>() != null) DestroyImmediate(GetComponent<NPCBody>());
+        InitializeNPCComponents();
+        // InitializePerceptionField();
+    }
+
+    #endregion
+
+    #region Initialization
+
+    private void InitializeNPCComponents() {
+        gBody = gameObject.AddComponent<NPCBody>();
+        gPerception = gameObject.AddComponent<NPCPerception>();
+        gAI = new NPCAI(this);
+        // hide flags
+            // gBody.hideFlags = HideFlags.HideInInspector;
+    }
+
+    #endregion
+}
---
+++ Application/Unity-Playground/Assets/Scripts/NPC/Utilities/NPCBody.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCBody : MonoBehaviour {
+
+    NavMeshAgent gAgent;
+    Animator gAnimator;
+
+    private static float m_OrientationRatio = 180f;
+
+    private Vector3 gForward;
+
+    #region Properties
+    public bool Navigation;
+    #endregion
+
+    [System.ComponentModel.DefaultValue(1f)]
+    private float MaxWalkSpeed { get; set; }
+
+    [System.ComponentModel.DefaultValue(2f)]
+    private float MaxRunSpeed { get; set; }
+
+    void Reset() {
+        Debug.Log("Initializing NPCBody ... ");
+        gAgent = GetComponent<NavMeshAgent>();
+        gAnimator = GetComponent<Animator>();
+        if (gAgent == null) {
+            gAgent = gameObject.AddComponent<NavMeshAgent>();
+            Debug.Log("NPCBody requires a NavMeshAgent if navigation is on, adding a default one.");
+        }
+        if (gAnimator == null) {
+            throw new System.Exception("NPCBody requires an Animator !!! ");
+        }
+    }
+
+    void Start() {
+        gForward = transform.forward;
+    }
+
+    public void UpdateBody() {
+
+    }
+
+}
